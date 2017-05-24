61235fbe97d4a1487de93ff304a12f5daae27e31
Richard Forrester <richard.forrester@gmail.com>
2017-05-24 01:40:17 +0000

    removed debugging console.log

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,7 +9,7 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      console.log(db)
+
 
         db.collection('tweets').insertOne(newTweet);
         callback(null, true);
